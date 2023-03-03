d50af2400035c62d17bac326b9bb7858bccf2d04
tkrupesh14 <info.tkrupesh@gmail.com>
2023-03-04 03:15:46 +0530

    donor page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,6 @@
 
 import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
-import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Link } from 'react-router-dom';
